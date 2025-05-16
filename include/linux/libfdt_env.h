d5cd833931ff08f6335ff737d1bec4e3f236374d
vlw <vlwwwwww@gmail.com>
2025-05-16 12:01:37 +0300

    dtc: fix build
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- include/linux/libfdt_env.h
+++ include/linux/libfdt_env.h
@@ -6,6 +6,18 @@
 
 #include <asm/byteorder.h>
 
+#ifdef __CHECKER__
+#define __force __attribute__((force))
+#define __bitwise __attribute__((bitwise))
+#else
+#define __force
+#define __bitwise
+#endif
+
+typedef uint16_t __bitwise fdt16_t;
+typedef uint32_t __bitwise fdt32_t;
+typedef uint64_t __bitwise fdt64_t;
+
 #define fdt32_to_cpu(x) be32_to_cpu(x)
 #define cpu_to_fdt32(x) cpu_to_be32(x)
 #define fdt64_to_cpu(x) be64_to_cpu(x)
